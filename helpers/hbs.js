4b332d7abdbdc7e49bafbc63559d8bac0e5ebf81
vermaashishasvp <vermaashishasvp@gmail.com>
2021-01-14 23:47:52 +0530

    update 2 profile

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -22,7 +22,7 @@ module.exports = {
       editIcon: function (storyUser, loggedUser, storyId, floating = true) {
         if (storyUser._id.toString() == loggedUser._id.toString()) {
           if (floating) {
-            return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fas fa-edit fa-small"></i></a>`
+            return `<a href="/stories/edit/${storyId}" class="btn-floating btn-small halfway-fab red"><i class="fas fa-edit fa-small"></i></a>`
           } else {
             return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`
           }
